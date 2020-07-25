757bf6e8d8804e5efd37e93671653303dc9c1f9b
tranmaihuong <tranmaihuonglhp@gmail.com>
2020-07-26 00:31:29 +0700

    can run

--- aws/classification.py
+++ aws/classification.py
@@ -2,21 +2,21 @@ import io
 import requests
 from io import BytesIO
 import pandas as pd
-response = requests.get('https://docs.google.com/spreadsheets/d/1ZPL7sejPopQVya3lE6F4-E1nPxJgqXaZ80fM-Xh-cDI/export?format=csv&id=1ZPL7sejPopQVya3lE6F4-E1nPxJgqXaZ80fM-Xh-cDI&gid=0')
-assert response.status_code == 200, 'Wrong status code'
-data = response.content
+files = requests.get('https://docs.google.com/spreadsheets/d/1ZPL7sejPopQVya3lE6F4-E1nPxJgqXaZ80fM-Xh-cDI/export?format=csv&id=1ZPL7sejPopQVya3lE6F4-E1nPxJgqXaZ80fM-Xh-cDI&gid=0')
+assert files.status_code == 200, 'Wrong status code'
+data = files.content
 
 # import data to dataframe
 df = pd.read_csv(BytesIO(data), usecols=['Name','Type']) #unprocessed data
 # print few rows
 df.head()
 print(df)
-object="Bottle"
+object=response["Labels"][0]["Name"]
 nrow,_ = df.shape
 #display circles represent for patients
 for i in range(0, nrow):
     name = df.iloc[i,0]
     if (str(name) == object):
         print(df['Type'][i])
-    else:
-        print("Not given")
+    # else:
+    #     print("Not given")
--- aws/rekognitionMerge.py
+++ aws/rekognitionMerge.py
@@ -23,3 +23,25 @@ MaxLabels=10,
 MinConfidence=95)
 # print(response["Labels"]) 
 # print(response["Labels"][0]["Name"])  
+import io
+import requests
+from io import BytesIO
+import pandas as pd
+files = requests.get('https://docs.google.com/spreadsheets/d/1ZPL7sejPopQVya3lE6F4-E1nPxJgqXaZ80fM-Xh-cDI/export?format=csv&id=1ZPL7sejPopQVya3lE6F4-E1nPxJgqXaZ80fM-Xh-cDI&gid=0')
+assert files.status_code == 200, 'Wrong status code'
+data = files.content
+
+# import data to dataframe
+df = pd.read_csv(BytesIO(data), usecols=['Name','Type']) #unprocessed data
+# print few rows
+df.head()
+print(df)
+object=response["Labels"][0]["Name"]
+nrow,_ = df.shape
+#display circles represent for patients
+for i in range(0, nrow):
+    name = df.iloc[i,0]
+    if (str(name) == object):
+        print(df['Type'][i])
+    # else:
+    #     print("Not given")
